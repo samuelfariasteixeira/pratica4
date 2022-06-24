80f7a5e45de5072c06a0d83159f728b1a264802b
unknown <samuel.teixeira@iesb.edu.br>
2022-06-23 23:14:39 -0300

    first commit

---
+++ app/src/main/java/com/example/atividade04/ListaActivity.kt
@@ -0,0 +1,32 @@
+package com.example.atividade04
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_lista)
+        setSupportActionBar(findViewById(R.id.tbrLista))
+        setTitle("Minhas Músicas")
+
+        val contatinhos = arrayOf("Isso é amor", "O amor me escolheu", "Selvagens")
+        var adaptador = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, contatinhos)
+
+        val lista = findViewById<ListView>(R.id.lstLista)
+        lista.adapter = adaptador
+
+        val botao = findViewById<FloatingActionButton>(R.id.fabAdicionar)
+        botao.setOnClickListener {
+            val intencao = Intent(applicationContext, CadastroActivity::class.java)
+            startActivity(intencao)
+        }
+    }
+
+}
